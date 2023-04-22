c7aeb10cc15aec4056cab15bae37a8e5c17acb1e
Juan Diego Jurado <juandiego_jp@hotmail.com>
2023-04-22 02:49:01 +0200

    CSS Navbar Usuarios

--- resources/views/gadiritas/navbar.blade.php
+++ resources/views/gadiritas/navbar.blade.php
@@ -5,7 +5,7 @@
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Gadirias</span>
         </a>
         <button data-collapse-toggle="navbar-default" type="button"
-            class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            class="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="navbar-default" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
@@ -24,7 +24,7 @@
                             <button>{{ $comarca->comarca }}</button>
                             <!-- Dropdown menu -->
                             <div class="comarca">
-                                <ul>
+                                <ul class="space-y-1">
                                     @foreach ($destinos as $destino)
                                         @if ($destino->comarca == $comarca->comarca)
                                             <li><a href="#">{{ $destino->nombre }}</a></li>
