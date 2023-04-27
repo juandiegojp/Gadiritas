0b40e5fe11e6b45004a1867aa7ff1828611f3e22
juandiegojp <juandiego.jurado@iesdonana.org>
2023-04-27 19:18:26 +0200

    Diseño de register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,14 +15,15 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased text-gray-900">
-        <div class="flex flex-col items-center min-h-screen pt-6 bg-gray-100 sm:justify-center sm:pt-0 dark:bg-gray-900">
+        <img src="https://wallpaperaccess.com/full/2688263.jpg" alt="" id="img-mapa">
+        <div class="flex flex-col items-center min-h-screen pt-6 sm:justify-center sm:pt-0">
             <div>
                 <a href="/">
                     <x-application-logo class="w-20 h-20 text-gray-500 fill-current" />
                 </a>
             </div>
 
-            <div class="w-full px-6 py-4 mt-6 overflow-hidden bg-white shadow-md sm:max-w-md dark:bg-gray-800 sm:rounded-lg">
+            <div class="w-full px-6 py-4 mt-6 overflow-hidden shadow-md sm:max-w-md dark: sm:rounded-lg" id="register">
                 {{ $slot }}
             </div>
         </div>
