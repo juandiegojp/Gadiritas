a26a356e64e198e086cc443de29ba76c2cc68267
Juan Diego Jurado <juandiego_jp@hotmail.com>
2023-04-30 03:13:09 +0200

    Welcome modificado. Footer.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,50 +22,7 @@
             </a>
         </div>
     </div>
-    <footer class="bg-white dark:bg-gray-900">
-        <div class="w-full max-w-screen-xl p-4 py-6 mx-auto lg:py-8">
-            <div class="flex justify-around mx-4 md:flex md:justify-between">
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Recursos</h2>
-                    <ul class="font-medium text-gray-600 dark:text-gray-400">
-                        <li class="mb-4">
-                            <a href="https://flowbite.com/" class="hover:underline">Flowbite</a>
-                        </li>
-                        <li>
-                            <a href="https://tailwindcss.com/" class="hover:underline">Tailwind CSS</a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Síguenos</h2>
-                    <ul class="font-medium text-gray-600 dark:text-gray-400">
-                        <li class="mb-4">
-                            <a href="https://github.com/juandiegojp/Gadiritas">
-                                <img src="{{ Vite::asset('resources/images/github.png') }}"
-                                    alt="Repositorio del proyecto.">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://linkedin.com/">
-                                <img src="{{ Vite::asset('resources/images/linkedin.png') }}"
-                                    alt="Repositorio del proyecto.">
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Contacto</h2>
-                    <ul class="font-medium text-gray-600 dark:text-gray-400">
-                        <li class="mb-4">
-                            <a href="#" class="hover:underline">
-                                <p>Contacto</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('gadiritas.footer')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
 </body>
 
