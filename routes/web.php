4cc89976112c33d780421f1849dccb345847c6be
Juan Diego Jurado <juandiego_jp@hotmail.com>
2023-05-02 01:27:56 +0200

    Perfil de usuario. Modificar datos personales, contraseña y borrar perfil.

--- routes/web.php
+++ routes/web.php
@@ -73,6 +73,9 @@ Route::group(['middleware' => ['auth']], function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
