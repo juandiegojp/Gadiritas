c683ce9a42ddcbb1ee86e2cf9ace8d4f92e257c2
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-02 10:55:38 +0200

    Cancelar reservas. Validación al cancelación.

--- resources/views/gadiritas/reservas.blade.php
+++ resources/views/gadiritas/reservas.blade.php
@@ -10,12 +10,15 @@
         oculto.setAttribute('value', id);
     }
 </script>
-    <div class="relative overflow-x-auto mx-4">
+    <div class="relative mx-4 overflow-x-auto">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th scope="col" class="px-6 py-3">
                         Actividad
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Fecha
                     </th>
                     <th scope="col" class="px-6 py-3">
                         Precio total
@@ -32,15 +35,24 @@
                             {{ $reserva->actividad->titulo }}
                         </td>
                         <td>
+                            {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/y') }} - {{ \Carbon\Carbon::parse($reserva->hora)->format('H:i') }}
+                        </td>
+                        <td>
                             {{ $reserva->personas * $reserva->actividad->precio }}€
                         </td>
                         <td>
                             <form action=" {{ route('usuarios.borrarReserva') }} " method="POST" class="inline">
                                 @csrf
                                 <input type="hidden" name="id" value="{{ $reserva->id }}">
+                                @if (\Carbon\Carbon::now()->diffInHours(\Carbon\Carbon::parse($reserva->created_at)) >= 24 && !\Carbon\Carbon::parse($reserva->fecha)->isPast())
                                 <button type="submit" onclick="cambiar(event, {{ $reserva->id }})"
-                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                    class="px-4 py-2 text-sm font-medium text-white bg-red-700 rounded-lg cursor-not-allowed focus:outline-none hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                    data-modal-toggle="popup-modal" disabled>Cancelar</button>
+                                @else
+                                <button type="submit" onclick="cambiar(event, {{ $reserva->id }})"
+                                    class="px-4 py-2 text-sm font-medium text-white bg-red-700 rounded-lg focus:outline-none hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
                                     data-modal-toggle="popup-modal">Cancelar</button>
+                                @endif
                             </form>
                         </td>
                     </tr>
