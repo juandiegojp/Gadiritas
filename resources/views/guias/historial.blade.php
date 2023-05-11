97ef0309c5edf2ff6f50b1c75eadb5e877406c9e
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-11 19:49:54 +0200

    Diseño de guias. Fondo.

--- resources/views/guias/historial.blade.php
+++ resources/views/guias/historial.blade.php
@@ -4,40 +4,44 @@
 @endsection
 @section('content')
     <p>HISTORIAL</p>
-    <div class="flex justify-center">
-        <div class="grid grid-cols-4 mx-4">
-            @foreach ($reservas as $reserva)
-                <a href="#"
-                    class="flex flex-col items-center w-3/4 my-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
-                        <h5 class="mb-2 text-sm font-bold tracking-tight text-gray-900">
-                            {{ $reserva->actividad->titulo }}</h5>
-                        <p class="mb-3 text-xs text-gray-700 ">Fecha de la reserva:
-                            {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
-                        </p>
-                        <p class="mb-3 text-xs text-gray-700 ">Hora:
-                            {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
-                        </p>
-                    </div>
-                </a>
-            @endforeach
-        </div>
+    <div class="relative flex items-center justify-center overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-3/4 mx-4 text-sm text-left text-gray-500">
+            <thead class="text-xs text-gray-600 uppercase bg-blue-100">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        #
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Título de la actividad
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Fecha
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Hora
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($reservas as $reserva)
+                    <tr
+                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $loop->iteration }}
+                        </td>
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $reserva->actividad->titulo }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
         {{ $reservas->links() }}
-    </div>
-
-    <div class="timeline"
-        style="margin: 0; padding: 0; font-family: var(--font); display: flex; justify-content: center;">
-        <div class="outer">
-            @foreach ($reservas as $reserva)
-            <div class="card">
-                <div class="info">
-                    <h3 class="title">{{ $reserva->actividad->titulo }}</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. </p>
-                </div>
-            </div>
-            @endforeach
-        </div>
     </div>
 @endsection
--- resources/views/guias/index.blade.php
+++ resources/views/guias/index.blade.php
@@ -55,43 +55,41 @@
             </button>
         </div>
     @endif
-
     <div class="flex justify-between mx-2">
         <div class="w-1/2 mt-4">
             <p class="inline-block">TODAY</p>
             @foreach ($reservasHoy as $reserva)
                 @if (\Carbon\Carbon::parse($reserva->hora)->isPast())
-                <a href="#"
-                class="hidden">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                    src="{{ Vite::asset("resources/images/{$reserva->actividad->id}.jpg") }}" alt="img">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                        {{ $reserva->actividad->titulo }}</h5>
-                    <p class="mb-3 font-normal text-gray-700 ">Fecha:
-                        {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
-                    <p class="mb-3 font-normal text-gray-700 ">Inicio:
-                        {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
-                    <p class="mb-3 font-normal text-gray-700 ">Nº de personas: {{ $reserva->personas }}
-                    </p>
-                </div>
-            </a>
+                    <a href="#" class="hidden">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                            src="{{ Vite::asset("resources/images/{$reserva->actividad->id}.jpg") }}" alt="img">
+                        <div class="flex flex-col justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                {{ $reserva->actividad->titulo }}</h5>
+                            <p class="mb-3 font-normal text-gray-700 ">Fecha:
+                                {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
+                            <p class="mb-3 font-normal text-gray-700 ">Inicio:
+                                {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
+                            <p class="mb-3 font-normal text-gray-700 ">Nº de personas: {{ $reserva->personas }}
+                            </p>
+                        </div>
+                    </a>
                 @else
-                <a href="#"
-                class="flex flex-col items-center my-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
-                <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                    src="{{ Vite::asset("resources/images/{$reserva->actividad->id}.jpg") }}" alt="img">
-                <div class="flex flex-col justify-between p-4 leading-normal">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                        {{ $reserva->actividad->titulo }}</h5>
-                    <p class="mb-3 font-normal text-gray-700 ">Fecha:
-                        {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
-                    <p class="mb-3 font-normal text-gray-700 ">Inicio:
-                        {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
-                    <p class="mb-3 font-normal text-gray-700 ">Nº de personas: {{ $reserva->personas }}
-                    </p>
-                </div>
-            </a>
+                    <a href="#"
+                        class="flex flex-col items-center my-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+                        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                            src="{{ Vite::asset("resources/images/{$reserva->actividad->id}.jpg") }}" alt="img">
+                        <div class="flex flex-col justify-between p-4 leading-normal">
+                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                {{ $reserva->actividad->titulo }}</h5>
+                            <p class="mb-3 font-normal text-gray-700 ">Fecha:
+                                {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
+                            <p class="mb-3 font-normal text-gray-700 ">Inicio:
+                                {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
+                            <p class="mb-3 font-normal text-gray-700 ">Nº de personas: {{ $reserva->personas }}
+                            </p>
+                        </div>
+                    </a>
                 @endif
             @endforeach
 
@@ -114,4 +112,6 @@
             </div>
             {{ $reservas->links() }}
         </div>
-    @endsection
+    </div>
+    </div>
+@endsection
