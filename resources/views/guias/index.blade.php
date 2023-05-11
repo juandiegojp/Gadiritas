228eeeaeb67feb2b394c93d24eab0ed87298f771
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-11 09:49:47 +0200

    Comentar .css

--- resources/views/guias/index.blade.php
+++ resources/views/guias/index.blade.php
@@ -67,7 +67,7 @@
                 <div class="flex flex-col justify-between p-4 leading-normal">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                         {{ $reserva->actividad->titulo }}</h5>
-                    <p class="mb-3 font-normal text-gray-700 ">Fecha de la reserva:
+                    <p class="mb-3 font-normal text-gray-700 ">Fecha:
                         {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
                     <p class="mb-3 font-normal text-gray-700 ">Inicio:
                         {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
