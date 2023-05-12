1f53001e444905fb71e982c8d818103bf42b098f
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-12 18:07:19 +0200

    Diseño dashboard guias

--- resources/views/guias/index.blade.php
+++ resources/views/guias/index.blade.php
@@ -21,17 +21,19 @@
             @foreach ($reservasHoy as $reserva)
                 @if (!\Carbon\Carbon::parse($reserva->hora)->isPast())
                     <a href="#"
-                        class="flex flex-col items-center my-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+                        class="flex flex-col items-center my-2 bg-white border border-gray-200 rounded-lg shadow pointer-events-none md:flex-row md:max-w-xl hover:bg-gray-100">
                         <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
                             src="{{ Vite::asset("resources/images/{$reserva->actividad->id}.jpg") }}" alt="img">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                                 {{ $reserva->actividad->titulo }}</h5>
-                            <p class="mb-3 font-normal text-gray-700 ">Fecha:
+                            <p class="mb-2 font-normal text-gray-700">Nº de personas: {{ $reserva->personas }}</p>
+                            <p class="mb-2 font-normal text-gray-700">Fecha:
                                 {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
-                            <p class="mb-3 font-normal text-gray-700 ">Inicio:
-                                {{ \Carbon\Carbon::parse($reserva->hora)->format('h:i') }}</p>
-                            <p class="mb-3 font-normal text-gray-700 ">Nº de personas: {{ $reserva->personas }}</p>
+                            <p class="mb-2 font-normal text-gray-700">Inicio:
+                                {{ \Carbon\Carbon::parse($reserva->hora)->format('H:i') }}</p>
+                            <p class="mb-2 font-normal text-gray-700"> Punto de encuentro:
+                                {{ $reserva->actividad->direccion }}</p>
                         </div>
                     </a>
                 @endif
@@ -40,23 +42,24 @@
         <div class="w-1/2 mt-4">
             <h1 class="flex items-center text-xl font-extrabold">Próximamente</h1>
             <div class="grid grid-cols-2" id="reservas">
-                @foreach ($reservas as $reserva)
-                    <a href="#" data-modal-target="modal{{ $reserva->id }}" data-modal-toggle="modal{{ $reserva->id }}"
+                @for ($i = 0; $i < count($reservas); $i++)
+                    <a href="#" data-modal-target="modal{{ $i }}"
+                        data-modal-toggle="modal{{ $i }}"
                         class="flex flex-col items-center w-3/4 my-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
                         <div class="flex flex-col justify-between p-4 leading-normal">
                             <h5 class="mb-2 text-sm font-bold tracking-tight text-gray-900">
-                                {{ $reserva->actividad->titulo }}</h5>
+                                {{ $reservas[$i]->actividad->titulo }}</h5>
                             <p class="mb-2 text-xs text-gray-700 ">Fecha de la reserva:
-                                {{ \Carbon\Carbon::parse($reserva->fecha)->format('d/m/Y') }}</p>
-                            @if ($reserva->personas == $reserva->actividad->max_personas)
-                                <p class="text-md text-gray-700 bg-yellow-100 text-center">¡COMPLETOS!</p>
+                                {{ \Carbon\Carbon::parse($reservas[$i]->fecha)->format('d/m/Y') }}</p>
+                            @if ($reservas[$i]->personas == $reservas[$i]->actividad->max_personas)
+                                <p class="text-center text-gray-700 bg-yellow-100 text-md">¡COMPLETOS!</p>
                             @else
-                                <p class="text-xs text-gray-700 ">Nº de personas: {{ $reserva->personas }}
+                                <p class="text-xs text-gray-700 ">Nº de personas: {{ $reservas[$i]->personas }}
                             @endif
                         </div>
                     </a>
                     <!-- Main modal -->
-                    <div id="modal{{ $reserva->id }}" tabindex="-1" aria-hidden="true"
+                    <div id="modal{{ $i }}" tabindex="-1" aria-hidden="true"
                         class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
                         <div class="relative w-full max-w-2xl max-h-full">
                             <!-- Modal content -->
@@ -64,11 +67,11 @@
                                 <!-- Modal header -->
                                 <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                                     <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                        {{$reserva->actividad->titulo}}
+                                        {{ $reservas[$i]->actividad->titulo }}
                                     </h3>
                                     <button type="button"
                                         class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                        data-modal-hide="modal{{ $reserva->id }}">
+                                        data-modal-hide="modal{{ $i }}">
                                         <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                             xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd"
@@ -80,15 +83,25 @@
                                 </div>
                                 <!-- Modal body -->
                                 <div class="p-6 space-y-6">
-                                    <p>{{$reserva->personas}}</p>
-                                    <p>{{$reserva->fecha}}</p>
-                                    <p>{{$reserva->hora}}</p>
-                                    <p>{{$reserva->actividad->direccion}}</p>
+                                    <p class="mb-2 font-normal text-gray-700">Nº de personas: {{ $reservas[$i]->personas }}
+                                        @if ($reservas[$i]->personas == $reservas[$i]->actividad->max_personas)
+                                            (¡COMPLETOS!)
+                                        @else
+                                            ({{ $reservas[$i]->actividad->max_personas - $reservas[$i]->personas }} plazas
+                                            disponibles)
+                                        @endif
+                                    </p>
+                                    <p class="mb-2 font-normal text-gray-700">Fecha:
+                                        {{ \Carbon\Carbon::parse($reservas[$i]->fecha)->format('d/m/Y') }}</p>
+                                    <p class="mb-2 font-normal text-gray-700">Hora:
+                                        {{ \Carbon\Carbon::parse($reserva->hora)->format('H:i') }}</p>
+                                    <p class="mb-2 font-normal text-gray-700">Dirección:
+                                        {{ $reservas[$i]->actividad->direccion }}</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                @endforeach
+                @endfor
             </div>
             {{ $reservas->links() }}
         </div>
