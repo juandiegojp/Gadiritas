1294c33b499b5583a0ff4457cc39e32f32a8eac4
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-16 11:18:59 +0200

    Seguimos con el diseño de Empleo.

--- resources/views/gadiritas/empleo.blade.php
+++ resources/views/gadiritas/empleo.blade.php
@@ -8,45 +8,51 @@
     <title>Gadiritas - Empleo</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="{{Vite::asset("resources/images/logoico.ico")}}" />
+    @vite(['resources/css/contacto.css'])
 </head>
 
 <body>
-    <h1>TRABAJA CON NOSOTROS</h1>
-    <form action="" method="POST" class="flex flex-col items-center justify-center mt-4"
-        enctype="multipart/form-data">
-        @csrf
-        <div class="w-1/2 mb-6">
-            <label for="nombre" class="block mb-2 text-sm font-medium text-gray-900">Nombre completo</label>
-            <input type="text" id="nombre" name="nombre"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                placeholder="Juan José Valladares del Olmo" required>
+    <div id="left-div">
+        <div id="semi-transparent-div">
+            <p>¡Únete a nuestro equipo!</p>
+            <h2>
+                Si te gusta el turisimo y enseñar a nuestros clientes todos y cada uno de los rincones, historia y actividades que
+                esconde nuestra provincia, no dudes y mándanos tu CV.
+            </h2>
         </div>
-        <div class="w-1/2 mb-6">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-            <input type="email" id="email" name="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                placeholder="tu@email.com" required>
-        </div>
-        <div class="w-1/2 mb-6">
-            <label for="tlf" class="block mb-2 text-sm font-medium text-gray-900">Teléfono</label>
-            <input type="tel" id="tlf" name="tlf"
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                placeholder="" required>
-        </div>
-        <div class="w-1/2 mb-6">
-            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Carta de
-                presentación</label>
-            <textarea id="message" rows="4" name="message"
-                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 resize-none"
-                placeholder="Breve descripción sobre ti..." required></textarea>
-        </div>
-        <div class="w-1/2 mb-6">
-            <label for="cv" class="block mb-2 text-sm font-medium text-gray-900">CV</label>
-            <input type="file" name="cv" id="cv" accept=".pdf" required>
-        </div>
-        <button type="submit"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar</button>
-    </form>
+    </div>
+    <div id="right-div">
+        <form action="" method="POST" enctype="multipart/form-data" id="trabajaNosotros">
+            @csrf
+            <div>
+                <label for="nombre">Nombre completo</label>
+                <input type="text" id="nombre" name="nombre" placeholder="Juan José Valladares del Olmo" required
+                    class="inputTrabajo">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="tu@email.com" required
+                    class="inputTrabajo">
+            </div>
+            <div>
+                <label for="tlf">Teléfono</label>
+                <input type="tel" id="tlf" name="tlf" placeholder="623451789" required class="inputTrabajo">
+            </div>
+            <div>
+                <label for="message">Carta de presentación</label>
+                <textarea id="message" rows="4" name="message"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 resize-none"
+                    placeholder="Breve descripción sobre ti..." required></textarea>
+            </div>
+            <div class="w-1/2 mb-6">
+                <label for="cv">Inserta aquí su CV:</label>
+                <input type="file" name="cv" id="cv" accept=".pdf" required
+                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400">
+            </div>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
 </body>
 
