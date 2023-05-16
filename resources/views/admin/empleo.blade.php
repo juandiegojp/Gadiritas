c52284ab38d97aad6ebd1002843254691905ffe6
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-16 13:07:21 +0200

    Diseño terminado (empleo). Ver más en carta de presentación.

--- resources/views/admin/empleo.blade.php
+++ resources/views/admin/empleo.blade.php
@@ -91,8 +91,16 @@
                         <td class="px-6 py-4">
                             {{ $cv->telefono }}
                         </td>
-                        <td class="px-6 py-4">
-                            {{ $cv->mensaje }}
+                        <td class="px-6 py-4 w-1/3">
+
+
+                            @if(strlen($cv->mensaje) > 50)
+                                {{ \Illuminate\Support\Str::limit($cv->mensaje, 50) }}
+                                <a href="#" data-modal-target="defaultModal{{ $cv->id }}"
+                                    data-modal-toggle="defaultModal{{ $cv->id }}">Ver más</a>
+                            @else
+                                {{ $cv->mensaje }}
+                            @endif
                         </td>
                         <td class="px-6 py-4">
                             <a href="{{ route('admin.cvs.download', $cv->id) }}"
@@ -100,6 +108,37 @@
                                 CV</a>
                         </td>
                     </tr>
+                    <div id="defaultModal{{ $cv->id }}" tabindex="-1" aria-hidden="true"
+                        class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative w-full max-w-2xl max-h-full">
+                            <!-- Modal content -->
+                            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                                <!-- Modal header -->
+                                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                        Carta de presentación de: {{ $cv->nombre }}
+                                    </h3>
+                                    <button type="button"
+                                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        data-modal-hide="defaultModal{{ $cv->id }}">
+                                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd"
+                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                clip-rule="evenodd"></path>
+                                        </svg>
+                                        <span class="sr-only">Close modal</span>
+                                    </button>
+                                </div>
+                                <!-- Modal body -->
+                                <div class="p-6 space-y-6">
+                                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                        {{ $cv->mensaje }}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             </tbody>
         </table>
