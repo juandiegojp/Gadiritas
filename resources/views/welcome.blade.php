96853f67774766d8810b138a1eaa6fa6ce4384be
Juan Diego Jurado <juandiego_jp@hotmail.com>
2023-05-21 19:13:40 +0200

    CSS: Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,15 +9,15 @@
     <title>Home</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.css" rel="stylesheet" />
-    @vite(['resources/css/gadiritas.css'])
+    @vite(['resources/css/gadiritas.css', 'resources/css/footer.css', 'resources/css/responsive.css'])
 </head>
 
 <body>
-    <div class="w-full h-screen flex items-center" id="welcomeDiv">
+    <div id="welcomeDiv">
         <div>
-            <p class="text-2xl font-extrabold">Busca, encuentra y ¡DISFRUTA!</p>
-            <p class="text-sm font-extrabold w-2/3 pb-2">Las mejores actividades de la provincia de Cádiz te esperan</p>
-            <a href=" {{ auth()->check() ? route('usuarios.index') : route('login') }} " class="py-2.5 px-5 text-sm font-medium focus:outline-none rounded-lg border focus:z-10 focus:ring-4 ring-gray-200 focus:ring-gray-700 bg-gray-800 text-white border-gray-600 hover:text-white hover:bg-gray-700">
+            <p>Busca, encuentra y ¡DISFRUTA!</p>
+            <p>Las mejores actividades de la provincia de Cádiz te esperan</p>
+            <a href=" {{ auth()->check() ? route('usuarios.index') : route('login') }} ">
                 Entrar
             </a>
         </div>
