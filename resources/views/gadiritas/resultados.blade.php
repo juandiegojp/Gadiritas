8b6685b6bad6e3818d2486f6d19710c537bc2754
juandiegojp <juandiego.jurado@iesdonana.org>
2023-05-23 19:56:22 +0200

    CSS: Portada resultados

--- resources/views/gadiritas/resultados.blade.php
+++ resources/views/gadiritas/resultados.blade.php
@@ -3,7 +3,30 @@
     Gadiritas - Búsqueda
 @endsection
 @section('content')
-<p>{{$ciudades[0]->nombre}}</p>
+    <div id="hero">
+        <nav class="flex" aria-label="Breadcrumb" id="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                <li>
+                    <div class="flex items-center">
+                        <a href="#"
+                            class="ml-1 md:ml-2">{{$ciudades[0]->comarca}}</a>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div class="flex items-center">
+                        <svg aria-hidden="true" class="w-6 h-6 text-black" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="ml-1 mr-4 md:ml-2">{{$ciudades[0]->nombre}}</span>
+                    </div>
+                </li>
+            </ol>
+        </nav>
+        <img src="{{ Vite::asset("resources/images/{$ciudades[0]->nombre}.jpg") }}" alt="{{ $ciudades[0]->nombre }}">
+    </div>
     <div id="resultadosContainer">
         <div id="filtroResultados">
             <form action="" method="POST">
